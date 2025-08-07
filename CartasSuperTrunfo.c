bb9d36ab9f21f38de2f20cbac36b4d48776a8ad6
fabvih <fabioallmeeida778@gmail.com>
2025-08-07 16:21:15 +0000

    criei scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
-int main(){
-    printf("Desafio cartas super trunfo!\n");
+int main(){    
+    printf("Paises!\n");
     printf("Novo commit\n");
 
+    int Populacao;
+    _Float16 Àrea;
+    _Float16  PIB;
+    int numeros pontos turisticos;
 
+    printf("digite sua populacao: \n");
+    scanf("%d", &Populacao);
 
-
+    printf("digite sua Àrea: \n");
+    scanf("%f", &Àrea);
 
 
 
