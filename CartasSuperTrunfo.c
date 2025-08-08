79f0e0a524d57a471c179d8ae97618e97c5da77d
fabvih <fabioallmeeida778@gmail.com>
2025-08-08 22:32:32 +0000

    criei A1 carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 
 int main(){    
+    printf("desafio cartas super trunfo\n");
     printf("Paises!\n");
     printf("Novo commit\n");
 
-    int Populacao;
-    _Float16 Àrea;
-    _Float16  PIB;
-    int numeros pontos turisticos;
+    printf("Digite o nome do estado: \n");
+    scanf("%s", &estadoA1);
+    
+    printf("Digite o codigo da carta: \n");
+    scanf("%49s", &codigoA1);
 
-    printf("digite sua populacao: \n");
-    scanf("%d", &Populacao);
+    printf("Digite o nome da cidade: \n");
+    scanf("%50s", &cidadeA1);
+    
+    printf("Digite a populacao da cidade: \n");
+    scanf("%d", &PopulacaoA1);
 
-    printf("digite sua Àrea: \n");
-    scanf("%f", &Àrea);
+    printf("Digite a area da cidade em km&sup2: \n");
+    scanf("%f", &Àreakm&sup2A1);
+
+    printf("Digite o PIB da cidade: \n");
+    scanf("%f", &PIBA1);
+
+    printf("Digite o numero de pontos turisticos da cidade: \n");
+    sacaf("%d", &turismoA1);
+    
 
 
 
